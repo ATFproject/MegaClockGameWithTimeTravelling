9ae3157c97fba9300e1e6707532389d039a51622
frtem2008 <ungazhiv2008@yandex.ru>
2024-06-19 12:54:28 +0300

    Added loading window properties from settings.json

---
+++ src/mcgwtt/SettingsLoader.h
@@ -0,0 +1,76 @@
+//
+// Created by Дом on 19.06.2024.
+//
+
+#ifndef MEGACLOCKGAMEWITHTIMETRAVELLING_SETTINGSLOADER_H
+#define MEGACLOCKGAMEWITHTIMETRAVELLING_SETTINGSLOADER_H
+
+#include "EngineDef.h"
+
+namespace mcgwtt {
+    inline sf::ContextSettings loadContextSettings() {
+        std::ifstream f("../bin/common/settings/settings.json");
+        auto context = json::parse(f)["context"];
+
+        sf::ContextSettings settings;
+        settings.majorVersion = context["OpenGL version"]["major"];
+        settings.minorVersion = context["OpenGL version"]["minor"];
+
+        settings.antialiasingLevel = context["antialiasing"];
+        settings.depthBits = context["depth bits"];
+        settings.stencilBits = context["stensil bits"];
+
+        return settings;
+    }
+
+    inline sf::VideoMode loadVideoMode() {
+        std::ifstream f("../bin/common/settings/settings.json");
+        auto window = json::parse(f)["window"]["mode"];
+        if (window["use fullscreen mode"])
+            return sf::VideoMode::getFullscreenModes()[0];
+        sf::VideoMode mode;
+        mode.width = window["width"];
+        mode.height = window["height"];
+        mode.bitsPerPixel = window["bits per pixel"];
+
+        return mode;
+    }
+
+    inline sf::Uint32 loadWindowStyle() {
+        std::ifstream f("../bin/common/settings/settings.json");
+        auto style = json::parse(f)["window"]["style"];
+        if (style["None"])
+            return sf::Style::None;
+        if (style["Default"])
+            return sf::Style::Default;
+        if (style["Fullscreen"])
+            return sf::Style::Fullscreen;
+
+        return style["Titlebar"].get<sf::Uint32>() |
+               style["Resize"].get<sf::Uint32>() |
+               style["Close"].get<sf::Uint32>();
+    }
+
+    inline std::string loadWindowTitle() {
+        std::ifstream f("../bin/common/settings/settings.json");
+        return json::parse(f)["window"]["title"];
+    }
+
+    inline void setWindowProperties(sf::RenderWindow &win) {
+        std::ifstream f("../bin/common/settings/settings.json");
+        auto window = json::parse(f)["window"];
+        sf::Image icon;
+        icon = engine::resourceHandler.loadRes(
+                engine::Texture::loadCommon(window["icon path"]))->getTex()->copyToImage();
+        win.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+        win.setPosition(sf::Vector2i(window["position"]["x"], window["position"]["y"]));
+        if (window["vertical sync"])
+            win.setVerticalSyncEnabled(true);
+        else
+            win.setFramerateLimit(window["framerate limit"]);
+    }
+
+}
+
+#endif //MEGACLOCKGAMEWITHTIMETRAVELLING_SETTINGSLOADER_H
--- src/mcgwtt/main.cpp
+++ src/mcgwtt/main.cpp
@@ -12,31 +12,24 @@
 
 #include "window/GameWindow.h"
 
+#include "SettingsLoader.h"
+
 int main() {
-    sf::ContextSettings winContextSettings(0, 0, 8);
-    winContextSettings.majorVersion = 4;
-    winContextSettings.minorVersion = 5;
     sf::RenderWindow win(
-            sf::VideoMode::getFullscreenModes()[0],
-            "MegaClockGameWithTimeTravelling 0.0.1 from ATFProject Game studio!",
-            sf::Style::Default,
-            winContextSettings);
+            mcgwtt::loadVideoMode(),
+            mcgwtt::loadWindowTitle(),
+            mcgwtt::loadWindowStyle(),
+            mcgwtt::loadContextSettings()
+    );
+    mcgwtt::setWindowProperties(win);
+    win.setActive(true);
 
     window::GameWindow window(&win);
-
-    sf::Image icon;
-    icon = engine::resourceHandler.loadRes(engine::Texture::loadCommon("icon.png"))->getTex()->copyToImage();
-    win.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-    win.setPosition(sf::Vector2i(0,0));
-    win.setFramerateLimit(165);
-    win.setActive(true);
 
     window.addGameObject(new mcgwtt::WindowController(&window), nullptr, nullptr);
 
     auto world = new mcgwtt::GameWorldPhysics(b2Vec2(0, 9.8), 1 / 165.f, 8, 3);
-
     window.addGameObject(nullptr, world, nullptr);
-
 
     auto view = new mcgwtt::ViewController();
     window.addGameObject(nullptr, nullptr, view);
@@ -49,7 +42,8 @@ int main() {
     else
         window.addGameObject(nullptr, nullptr, new mcgwtt::DebugCamera(view, 30));
 
-    window.addGameObject(nullptr, new mcgwtt::PlayerPhysics(world, 0, 0),
+    window.addGameObject(nullptr,
+                         new mcgwtt::PlayerPhysics(world, 0, 0),
                          new mcgwtt::PlayerGraphics(&win));
 
     window.addGameObject(nullptr,
