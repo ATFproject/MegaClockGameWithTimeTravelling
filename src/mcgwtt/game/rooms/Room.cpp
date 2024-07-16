e5faefc9b4ec815e8ad0230dc4817a17088e2dbb
Тимур Минажетдинов <timmin2303@mail.ru>
2024-07-16 16:51:18 +0300

    Rooms classes added, first implementation.

---
+++ src/mcgwtt/game/rooms/Room.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2024. ATF project organization!
+
+//
+// Created by Timmimin on 08.07.2024.
+//
+
+#include <string>
+#include <fstream>
+#include "Room.h"
+#include "EngineDef.h"
+
+void mcgwtt::Room::load_from_file(const std::string &BinFileName) {
+    const std::string path = "../bin/Rooms JSONs/" + BinFileName;
+    std::ifstream f(path, std::ios::in);
+    json loadedRoom(json::parse(f));
+    // Fill data
+    loadedRoom.at("name").get_to(Name);
+    loadedRoom.at("width").get_to(Width);
+    loadedRoom.at("height").get_to(Height);
+
+    loadedRoom.at("numOfExits").get_to(NumOfExits);
+    if (NumOfExits != 0) {
+        Exits = new RoomExit[NumOfExits];
+    }
+}
+
+bool mcgwtt::Room::add_neighbour(mcgwtt::Room *Neighbour, const int &NeighbourExitNumToConnect,
+                                 const int &ExitNumToConnect) {
+    // Check if out of array size
+    if (NeighbourExitNumToConnect < 0 || NeighbourExitNumToConnect >= Neighbour->NumOfExits
+        || ExitNumToConnect < 0 || ExitNumToConnect >= NumOfExits) {
+        return false;
+    }
+    // Check if out of space left
+    if (Exits[ExitNumToConnect].Neighbour != nullptr) {
+        return false;
+    }
+
+    Exits[ExitNumToConnect].Neighbour = Neighbour;
+    Exits[ExitNumToConnect].nebX = Neighbour->Exits[NeighbourExitNumToConnect].x;
+    Exits[ExitNumToConnect].nebY = Neighbour->Exits[NeighbourExitNumToConnect].y;
+}
--- src/mcgwtt/game/rooms/Room.h
+++ src/mcgwtt/game/rooms/Room.h
@@ -8,7 +8,41 @@
 #define MEGACLOCKGAMEWITHTIMETRAVELLING_ROOM_H
 
 namespace mcgwtt {
+    class Room; // Forward declaration
+
+    enum class RoomExitType {
+        DEFAULT,
+        WALL,
+        SIDED_UP,
+        SIDED_DOWN,
+        SIDED_LEFT,
+        SIDED_RIGHT,
+        /*Smth else*/
+    };
+
+    class RoomExit {
+    public:
+      uint x = 0, y = 0;
+      uint nebX = 0, nebY = 0;
+      RoomExitType Type = RoomExitType::DEFAULT;
+      Room *Neighbour = nullptr;
+    };
+
     class Room {
+    private:
+        std::string Name = "Undefined";
+        uint Width = 0, Height = 0;
+
+    public:
+        uint NumOfExits = 0; // Same as "NeighbourRooms" array size
+        RoomExit *Exits = nullptr;
+        Room() = default;
+        ~Room() {
+            delete[] Exits;
+        }
+
+        void load_from_file(const std::string &BinFileName);
+        bool add_neighbour(Room *Neighbour, const int &NeighbourExitNumToConnect, const int &ExitNumToConnect);
     };
 }
 
